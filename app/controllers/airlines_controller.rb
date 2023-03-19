005472fa84cb2ee4c39882fb1e433c8542177559
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-19 11:09:15 +0300

    CRUD in controllers

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,2 +1,51 @@
 class AirlinesController < ApplicationController
+
+        # GET 
+      def index
+        airlines = Airline.all
+        render json: airlines , status: 200
+      end
+      
+      # GET 
+      def show
+        
+      end
+
+      # POST 
+      def create
+        airline = Airline.create!(airline_params)
+
+        if airline.save
+          render json: airline
+        else
+          render json: {errors:"airline not  created"}, status: unprocessable_entity
+        end
+      end
+
+      # PATCH 
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.update(airline_params)
+          render json:airline
+        else
+          render json: {errors:"airline not found"}, status:not_found
+        end
+      end
+
+      # DELETE 
+      def destroy
+        if airline.destroy
+          head :no_content
+        else
+          render json: {errors:"airline cannot be found"}, status: not_found
+        end
+      end
+
+      private
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
+
+    
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+    def create
+        review = Review.create!(review_params)
+
+        if review.save
+          render json: review
+        else
+          render json: {errors:"could not create review"}, status: unprocessable_entity
+        end
+      end
+
+      # DELETE 
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json:{errors:"could not create review"} , status: unprocessable_entity
+        end
+      end
+
+      private
+
+     
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
 end
